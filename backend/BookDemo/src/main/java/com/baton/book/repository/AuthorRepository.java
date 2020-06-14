29ffcd4faa0c42ec4dd46b3db81102a494bfaa75
adarsh <gupta.adrash1497@gmail.com>
2020-06-14 14:56:48 +0530

    removed files

--- backend/BookDemo/src/main/java/com/baton/book/repository/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.baton.book.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.baton.book.model.Author;
-
-public interface AuthorRepository extends CrudRepository<Author, Integer> {
-
-}
--- backend/BookDemo/src/main/java/com/baton/book/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.baton.book.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.baton.book.model.Book;
-
-public interface BookRepository extends CrudRepository<Book, Integer> {
-
-}
